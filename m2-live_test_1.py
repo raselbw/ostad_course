19f7f2f6c666e5fdda06879622d2bf5e8e10a910
Rasel Biswas <rbiswas1@isrt.ac.bd>
2025-05-25 23:09:17 +0600

    added m3 a1

---
+++ m2-live_test_1.py
@@ -0,0 +1,7 @@
+# Write a function to make an hour-to-minute converter.
+
+def hour_to_minute(hours):
+    return hours*60
+
+print(hour_to_minute(hours=5))
+
--- p123.py
+++ p123.py
@@ -1,24 +1,17 @@
-li = [1, 2, 3]
-set1 = set()
+a = input("Enter elements of Set A separated by spaces: ")
+a = set(a.split())
+a = {int(i) for i in a}
 
-set1.add
-li.append
+b = input("Enter elements of Set B separated by spaces: ")
+b = set(b.split())
+b = {int(i) for i in b}
 
-type(li)
+a_union_b = a | b
+a_intersection_b = a & b
+a_diff_b = a - b
+b_diff_a = b - a
 
-li.copy()
-
-li.append(23)
-
-li[3] = 45
-print(li)
-
-len(li)
-
-li.
-
-li.count(2)
-
-li.sort()
-
-
+print("Union:", a_union_b)
+print("Intersection:", a_intersection_b)
+print("A-B:", a_diff_b)
+print("B-A:", b_diff_a)
